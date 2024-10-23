319351309fe3bf17e125bfceb5e5b3fd240aa8dd
Vanitzza <vanitzzacruz@gmail.com>
2024-10-23 17:36:52 -0600

    Testimonials
    Testing testimonial as a part

---
+++ parts/testimonials.php
@@ -0,0 +1,83 @@
+<!-- wp:cover {"url":"https://best-day-ever.web.dmitcapstone.ca/wordpress2/wp-content/uploads/2024/10/testimonial-bg.webp","id":225,"dimRatio":0,"overlayColor":"cream","isUserOverlayColor":true,"isDark":false,"tagName":"section","metadata":{"name":"Testimonials"},"style":{"spacing":{"padding":{"top":"64px","bottom":"64px","left":"64px","right":"64px"}}},"layout":{"type":"constrained"}} -->
+<section class="wp-block-cover is-light" style="padding-top:64px;padding-right:64px;padding-bottom:64px;padding-left:64px"><span aria-hidden="true" class="wp-block-cover__background has-cream-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-225" alt="" src="https://best-day-ever.web.dmitcapstone.ca/wordpress2/wp-content/uploads/2024/10/testimonial-bg.webp" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Inner Content"},"style":{"spacing":{"blockGap":"0px"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained","contentSize":"600px","wideSize":"1200px"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","align":"wide","style":{"typography":{"fontSize":"44px","fontStyle":"normal","fontWeight":"400","textTransform":"capitalize","letterSpacing":"0px","lineHeight":"1"},"spacing":{"padding":{"left":"0px"},"margin":{"bottom":"16px"}}},"textColor":"dark-red"} -->
+<h2 class="wp-block-heading alignwide has-text-align-left has-dark-red-color has-text-color" style="margin-bottom:16px;padding-left:0px;font-size:44px;font-style:normal;font-weight:400;letter-spacing:0px;line-height:1;text-transform:capitalize">What Our Travelers are Saying</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"16px","bottom":"16px"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:16px;margin-bottom:16px"><!-- wp:column {"width":"100%","metadata":{"name":"Testimonial 1"},"style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-column" style="padding-top:0px;padding-bottom:0px;flex-basis:100%"><!-- wp:cover {"overlayColor":"dark-orange","isUserOverlayColor":true,"contentPosition":"center center","metadata":{"name":"Backdrop"},"className":".test-backdrop","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover .test-backdrop" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><span aria-hidden="true" class="wp-block-cover__background has-dark-orange-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:cover {"overlayColor":"white","isUserOverlayColor":true,"contentPosition":"center center","isDark":false,"metadata":{"name":"Testimonial Card"},"className":".testimonial-card","style":{"border":{"radius":"10px","color":"#33050725","width":"2px"},"shadow":"var:preset|shadow|natural"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light .testimonial-card has-border-color" style="border-color:#33050725;border-width:2px;border-radius:10px;box-shadow:var(--wp--preset--shadow--natural)"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="post_title" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:shortcode -->
+[vg_display_data key="post_excerpt" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="_thumbnail_id" object_id="1" template="<b>Image url:</b> {{var}}" flag="file_url"]
+<!-- /wp:shortcode -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="authors_name" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:shortcode -->
+[vg_display_data key="authors_vacation" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"15%"} -->
+<div class="wp-block-column" style="flex-basis:15%"></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%","metadata":{"name":"Testimonial 2"}} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:cover {"overlayColor":"dark-orange","isUserOverlayColor":true,"metadata":{"name":"Backdrop"},"className":".test-backdrop","style":{"spacing":{"padding":{"top":"0px","bottom":"0px","left":"0px","right":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover .test-backdrop" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><span aria-hidden="true" class="wp-block-cover__background has-dark-orange-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:cover {"overlayColor":"white","isUserOverlayColor":true,"contentPosition":"center center","isDark":false,"metadata":{"name":"Testimonial Card"},"className":".testimonial-card","style":{"border":{"radius":"10px","color":"#33050725","width":"2px"},"shadow":"var:preset|shadow|natural"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light .testimonial-card has-border-color" style="border-color:#33050725;border-width:2px;border-radius:10px;box-shadow:var(--wp--preset--shadow--natural)"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="post_title" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:shortcode -->
+[vg_display_data key="post_excerpt" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="_thumbnail_id" object_id="1" template="<b>Image url:</b> {{var}}" flag="file_url"]
+<!-- /wp:shortcode -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:shortcode -->
+[vg_display_data key="authors_name" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode -->
+
+<!-- wp:shortcode -->
+[vg_display_data key="authors_vacation" object_id="1" data_source="post_data"]
+<!-- /wp:shortcode --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"metadata":{"name":"Buttons"},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:post-navigation-link {"type":"previous"} /-->
+
+<!-- wp:post-navigation-link /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></section>
+<!-- /wp:cover -->
