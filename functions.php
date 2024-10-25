739681c7fec5a8bfa10e3c2aa21e05361d9d001a
Ella Bach <chipbach294@gmail.com>
2024-10-25 14:12:48 -0600

    change the html element for page.php

--- functions.php
+++ functions.php
@@ -247,8 +247,8 @@ add_action('widgets_init', 'bdev_by_jen_widgets_init');
 function bdev_by_jen_scripts()
 {
 	wp_enqueue_style('bdev_by_jen-style', get_stylesheet_uri(), array(), _S_VERSION);
-	wp_enqueue_style('bdev_by_jen-main', get_template_directory_uri() . '/css/main.css');
-	wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
+	// wp_enqueue_style('bdev_by_jen-main', get_template_directory_uri() . '/css/main.css');
+	// wp_enqueue_style('bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
 
 	wp_style_add_data('bdev_by_jen-style', 'rtl', 'replace');
 
