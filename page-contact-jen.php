1f2e188e9a86ba1a6a558fb71beaf5b70fdde6df
Ella Bach <chipbach294@gmail.com>
2024-10-27 12:54:09 -0600

    change the html layout section

--- page-contact-jen.php
+++ page-contact-jen.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for display the Contact Jen page only
  *
@@ -10,28 +11,23 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<main id="primary" class="site-main">
+    <section>
+        <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        <?php the_content(); ?>
 
-		<section>
-			<?php the_content(); ?>
+        <?php if (is_active_sidebar('accordion-faqs')) {
+            dynamic_sidebar('accordion-faqs');
+        };
+        ?>
 
-            <section class="accordion">    
-                <?php if (is_active_sidebar('accordion-faqs')) {
-                        dynamic_sidebar('accordion-faqs');
-                    }; 
-                ?>
-            </section>
+        <?php if (is_active_sidebar('subscription-main')) {
+            dynamic_sidebar('subscription-main');
+        };
+        ?>
+    </section>
 
-            <section class="subscription">
-                <?php if (is_active_sidebar('subscription-main')) {
-                        dynamic_sidebar('subscription-main');
-                    }; 
-                ?>
-            </section>
-		</section>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
