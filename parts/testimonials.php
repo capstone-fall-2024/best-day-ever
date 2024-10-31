06c3c6a5c86820215afb00dce4f007e7cbad1b98
Vanitzza <vanitzzacruz@gmail.com>
2024-10-31 16:10:19 -0600

    Front page edits
    Edited front-page for html - testimonials commented out since we'll be approaching it differently for now

---
+++ parts/testimonials.php
@@ -0,0 +1,18 @@
+<!-- <section>
+    <div class="">
+        <h2>What Our Travelers are Saying</h2>
+        <div>
+        <?php
+        
+        
+        
+        ?>
+            <section>
+                <div>
+                  
+                </div>
+            </section>
+
+        </div>
+    </div>
+</section> -->
