bf9aae05f2423b0f99b05ed5793c04771c48d98c
Vanitzza <vanitzzacruz@gmail.com>
2024-12-01 20:45:34 -0700

    Favicon
    Added icon for site tab

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="icon" href="./img/bdev-logo.png" type="image/png">
 
 	<!-- Google Fonts -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
